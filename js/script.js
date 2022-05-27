08a71fb04ff093645c270b3d659e7dded7ae061e
AlexandrST228 <99550169+AlexandrST228@users.noreply.github.com>
2022-05-28 00:12:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,10 @@ $('.pop-up span').click(function () {
    $('.pop-up').hide(0);
    $('.pop-up-button').show(0);
 });
+
+function chpok(id) {
+   elem = document.getElementById(id); //находим блок div по его id, который передали в функцию
+   state = elem.style.display; //смотрим, включен ли сейчас элемент
+   if (state == '') elem.style.display = 'block'; //если включен, то выключаем
+   else elem.style.display = ''; //иначе - включаем
+}
